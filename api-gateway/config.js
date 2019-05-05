17a553af87699eb3d8a8b0489d9302eb0a0ae51b
Drew Hanson <drew.o.hanson@gmail.com>
2019-05-05 12:34:40 -0600

    assignment 1.4

---
+++ api-gateway/config.js
@@ -0,0 +1,17 @@
+/*
+============================================
+; Title:  config.js
+; Author: Professor Krasso
+; Date:  5 May 2019
+; Modified By: Drew Hanson
+; Description: Configures the server
+;===========================================
+*/
+
+var config = {};
+
+config.web = {};
+
+config.web.port = process.env.PORT || '3000';
+
+module.exports = config;
