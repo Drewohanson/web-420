cce5538215a9f9ae9c2cdfafaaf4d6db32657e04
Drew Hanson <drew.o.hanson@gmail.com>
2019-05-25 22:52:58 -0600

    week 4

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -11,28 +11,31 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-var indexRouter = require('./routes/index');
-var apiCatalog = require('./routes/api-catalog')
+
+var index = require('./routes/index');
+var apiCatalog = require('./routes/api-catalog');
 
 var app = express();
-
+  
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/api', apiCatalog); 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', index);
+app.use('/api', apiCatalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -16,7 +16,18 @@ var userSchema = new mongoose.Schema({
  password: String,
  email: String
 });
-module.exports = mongoose.model('User', userSchema);
+
+const user = module.exports = mongoose.model('User', userSchema);
 /**
 Database queries
 */
+ // user.save is used to add a new user in our database
+ module.exports.add = (user, callback) => {
+    user.save(callback);
+};
+
+// get by id
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+};
